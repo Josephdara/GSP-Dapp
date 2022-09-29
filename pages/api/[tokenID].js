ffb81dd7e8de1843c3fb9034905537ac66ba9954
Joseph Dara <jsatimehin@gmail.com>
2022-09-29 19:07:15 +0100

    add img

---
+++ pages/api/[tokenID].js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  const tokenID = req.query.tokenID;
+  const name = `Glory Sound Prep #${tokenID}`
+  const description = "Glory Sound prep NFTs represented as Punks"
+  const image = ""
+
+}
