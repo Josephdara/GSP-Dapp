81dccbd14c574ced22ac3254197d22a2db642f47
Joseph Dara <jsatimehin@gmail.com>
2022-09-30 10:48:48 +0100

    roll back

--- pages/api/[tokenID].js
+++ pages/api/[tokenID].js
@@ -2,18 +2,17 @@ export default function handler(req, res) {
   const tokenID = req.query.tokenID;
   const name = `Glory Sound Prep #${tokenID}`;
   const description = "Glory Sound prep NFTs represented as Punks";
-  const image = `https://raw.githubusercontent.com/Josephdara/GSP-Dapp/main/public/images/${
-    Number(tokenID)
-  }.png`;
-  const cid = `https://raw.githubusercontent.com/Josephdara/GSP-Dapp/main/public/json/${
-    Number(tokenID)
-  }.json`;
-  const attributes = [cid]
+  const image = `https://raw.githubusercontent.com/Josephdara/GSP-Dapp/main/public/images/${Number(
+    tokenID
+  )}.png`;
+  const attributes = `https://raw.githubusercontent.com/Josephdara/GSP-Dapp/main/public/json/${Number(
+    tokenID
+  )}.json`;
 
   return res.json({
-    "name": name,
-    "description": description,
-    "image": image,
-    "attributes": attributes,
+    name: name,
+    description: description,
+    image: image,
+    attributes: attributes,
   });
 }
